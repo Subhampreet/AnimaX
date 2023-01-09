a36d852c98043c2e85317e55be5ec9cf799a2c28
Subhampreet <subhampreet18@gmail.com>
2023-01-09 19:07:53 +0530

    Sidebar Update

--- components/App.js
+++ components/App.js
@@ -3,29 +3,38 @@ import AnimeCard from "./Anime/AnimeCard";
 
 function App() {
   const [animeList, setAnimeList] = useState([]);
-  const [topAnime, setTopAnime] = useState([])
+  const [topAnime, setTopAnime] = useState([]);
   const [search, setSearch] = useState("");
 
   const getTopAnime = async () => {
-    const temp = await fetch(
-      `https://api.jikan.moe/v4/top/anime`
-    ).then(res => res.json());
+    const temp = await fetch(`https://api.jikan.moe/v4/top/anime`).then((res) =>
+      res.json()
+    );
     setTopAnime(temp.data);
   };
 
   useEffect(() => {
-    getTopAnime()
+    getTopAnime();
 
-    console.log(topAnime)
+    console.log(topAnime);
   }, []);
 
   return (
-    <div className="anime-main">
-      {
-        topAnime.map(anime => (
-          <AnimeCard id={anime.mal_id} title={anime.title} summary={anime.synopsis} image_url={anime.images.jpg.image_url} rating={anime.rating}/>
-        ))
-      }
+    <div className="App">
+      {/* <div className="banner">
+        <img src="https://staticg.sportskeeda.com/editor/2022/10/d8581-16663316932144-1920.jpg" />
+      </div> */}
+      <div className="anime-main">
+        {topAnime.map((anime) => (
+          <AnimeCard
+            id={anime.mal_id}
+            title={anime.title}
+            summary={anime.synopsis}
+            image_url={anime.images.jpg.image_url}
+            rating={anime.rating}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ components/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+        <div className='body'>
+            <div className='logo'>AnimaX</div>
+            <div className='menu'>
+                <div className='menu-item'>TOP ANIME</div>
+                <div className='menu-item'>ROMANCE</div>
+                <div className='menu-item'>ACTION</div>
+                <div className='menu-item'>TEEN</div>
+            </div>
+        </div>        
+    </div>
+  )
+}
+
+export default Sidebar
