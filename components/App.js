256b1a7b5b2c27dd09bc3b5764104bd0487aed56
Subhampreet <subhampreet18@gmail.com>
2023-01-11 00:17:27 +0530

    Search Functionality Added

--- components/App.js
+++ components/App.js
@@ -3,12 +3,13 @@ import AnimeCard from "./Anime/AnimeCard";
 import { BiSearch } from "react-icons/bi";
 
 function App() {
-  const [animeList, setAnimeList] = useState([]);
+  const [search, setSearch] = useState("naruto");
+  const [animeSearchList, setAnimeSearchList] = useState([]);
   const [topAnime, setTopAnime] = useState([]);
-  const [search, setSearch] = useState("");
+  
 
   const getTopAnime = async () => {
-    const temp = await fetch(`https://api.jikan.moe/v4/top/anime`).then((res) =>
+    const temp = await fetch(`https://api.jikan.moe/v4/anime?q=${search}&sfw`).then((res) =>
       res.json()
     );
     setTopAnime(temp.data);
@@ -18,24 +19,26 @@ function App() {
     getTopAnime();
 
     console.log(topAnime);
-  }, []);
+  }, [search]);
 
   return (
-    <div className="App">
-      {/* <div className="banner">
-        <img src="https://staticg.sportskeeda.com/editor/2022/10/d8581-16663316932144-1920.jpg" />
-      </div> */}
+    <div className="App">      
       <div className="header">
         <div className="left">
           <div className="search-box">
             <BiSearch className="search-icon" />
-            <input type="search" placeholder="Search anything" />
+            <input type="search" placeholder="Search anything" onChange={(e) => setSearch(e.target.value)} />
           </div>
         </div>
         <div className="right"></div>
       </div>
+
+      <div className="banner">
+        <img src="https://staticg.sportskeeda.com/editor/2022/10/d8581-16663316932144-1920.jpg" />
+      </div>
+
       <div className="anime-main">
-        {topAnime.map((anime) => (
+        {topAnime?.map((anime) => (
           <AnimeCard
             id={anime.mal_id}
             title={anime.title}
